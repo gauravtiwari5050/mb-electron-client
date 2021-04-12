f8d09e50d8433b52a089aeb715e45bb33e023e9c
Gaurav Tiwari <gauravtiwari5050@gmail.com>
2021-04-12 06:37:41 -0700

    clean distribution files

--- dist/services/magic_bell/IMagicBellNotificationReceiver.d.ts
+++
@@ -1,3 +0,0 @@
-export interface IMagicBellEventReceiver {
-    receive(): any;
-}
--- dist/services/native_notifcation/fakes/FakeNativeNotificationService.d.ts
+++
@@ -1,5 +0,0 @@
-import { INativeNotificationService, INativeNotificationPayload } from "../INativeNotificationService";
-import { Result } from "../../../util/Result";
-export declare class FaleNativeNotificationService implements INativeNotificationService {
-    send(payload: INativeNotificationPayload): Promise<Result<boolean>>;
-}
